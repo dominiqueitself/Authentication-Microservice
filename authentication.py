c5fd585f58dad4cb0a63eb4fd1145b7db6c2f7b4
dominiqueitself <132427270+dominiqueitself@users.noreply.github.com>
2024-08-07 15:17:19 +0800

    added db and functions

--- authentication.py
+++ authentication.py
@@ -1,17 +1,40 @@
 from flask import Flask, jsonify, request
 import json
+import mysql.connector
 
 app = Flask(__name__)
 
-employees = [ 
-    { 'id': 1, 'name': 'Ashley' }, 
-    { 'id': 2, 'name': 'Kate' }, 
-    { 'id': 3, 'name': 'Joe' }
-    ]
+authdb = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = "root",
+    database = "authenticationMicroservice"
+)
 
-@app.route('/employees', methods=['GET'])
-def get_employees():
-    return jsonify(employees)
+authcursor = authdb.cursor()
+
+def get_employees(email, password):
+
+    query = "SELECT email, password, authorization FROM employees WHERE email = %s AND password = %s"
+    authcursor.execute(query, (email, password))
+    employee = authcursor.fetchone()
+    authdb.close()
+    
+    if employee:
+        return jsonify({"success": TRUE, "authorization": employee[2]})    
+    else:
+        return jsonify({"failed": FALSE})
+
+@app.route('/authentication', methods=['POST'])
+def authenticate():
+    data = request.get_json()
+    email = data.get('email')
+    password = data.get('password')
+    return get_employees(email, password)
+
+#need pa itesting
+#need pa ideploy
+#need pa makuha yung input - backend sa login tapos isend sa dito yung email at password para macheck
 
 if __name__ == '__main__':
     app.run(debug=True)
