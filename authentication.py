8f60346b822343cd9285e4c158592cfc1da5e76a
dominiqueitself <132427270+dominiqueitself@users.noreply.github.com>
2024-08-17 09:04:55 +0800

    added renz updates

--- authentication.py
+++ authentication.py
@@ -3,6 +3,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 import mysql.connector as mysql
 import json
 import jwt
+import pytz
 import datetime 
 
 app = Flask(__name__)
@@ -25,12 +26,14 @@ authdb = mysql.connect(
 )
 authcursor = authdb.cursor()
 
+# FUNCTIONS
+
 #function - for generating a token
-def generate_token(username, authorizationId):
+def generate_token(username, roleName):
     payload = {
         "username": username,
-        "authorization ID": authorizationId,
-        "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=30),
+        "role": roleName,
+        "exp": datetime.datetime.now(pytz.utc) + datetime.timedelta(minutes=30),
         "Content-Type": "application/json",
     }
     token = jwt.encode(payload=payload, key=secret_key, algorithm="HS256")
@@ -38,19 +41,49 @@ def generate_token(username, authorizationId):
 
 #function - for checking the user's username and password
 def check_employees(username, password):
-    authcursor.execute("SELECT username, hashPassword, authorizationId FROM USERS WHERE username = %s", (username,) )
+    sql = """SELECT u.username, u.hashPassword, a.roleName 
+             FROM USERS u
+             JOIN AUTHORIZATIONS a ON u.authorizationId = a.authorizationId
+             WHERE u.username = %s"""
+    
+    authcursor.execute(sql, (username,))
     employee = authcursor.fetchone()
 
     if employee and check_password_hash(employee[1], password): 
-        username, authorizationId = employee
-        token = generate_token(username, authorizationId)
+        username, roleName = employee
+        token = generate_token(username, roleName)
         return jsonify({"token": token}), 200
     elif employee:
         return jsonify({"error": 'Invalid credentials'}), 401
     else:
         return jsonify({"error": 'Unexpected Error'})
 
-#API route for checking the user's credential and sending out result
+
+# API ROUTES
+
+# for verifying the token of the user to access certain modules
+@app.route('/verify-token', methods=['POST'])
+def verify_token():
+    data = request.get_json()
+    token = data.get('token')
+
+    try:
+        # Decode the token
+        decoded_token = jwt.decode(token, secret_key, algorithms=["HS256"])
+
+        # Return the username and role name instead of ID
+        return jsonify({
+            "username": decoded_token["username"],
+            "role": decoded_token["role"] 
+        }), 200
+
+    except jwt.ExpiredSignatureError:
+        return jsonify({"error": "Token has expired"}), 401
+    except jwt.InvalidTokenError:
+        return jsonify({"error": "Invalid token"}), 401
+
+
+# for checking the user's credential and sending out result
 @app.route('/authenticate', methods=['POST'])
 def authenticate():
     data = request.get_json()
@@ -58,6 +91,7 @@ def authenticate():
     password = data.get('password')
     return check_employees(username, password)
 
+# for locking the users account when login attempts have exceeded
 @app.route('/accountLocked', methods=['POST'])
 def accountLocked():
     data = request.get_json()
@@ -65,11 +99,11 @@ def accountLocked():
     authcursor.execute("UPDATE USERS SET accountLocked = TRUE WHERE username = %s", (username,))
     return True
 
-""" just for adding users in the database
+"""
 @app.route('/')
 def generate_password():
-    password = generate_password_hash("maingayKAYO")
-    values = ("dominique", password, "shanejain00@gmail.com", '09213373131', 2)
+    password = generate_password_hash("helloWorld")
+    values = ("renz", password, "renzfb@gmail.com", '09213373122', 1)
 
     sql = "INSERT INTO USERS (username, hashPassword, email, contactNumber, authorizationId) VALUES (%s, %s, %s, %s, %s)"
     authcursor.execute(sql, values)
